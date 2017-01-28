9a09048c0746ef0a2495f1bad6aba4f8ce0453d8
shettysandeepr <shettysandeepr@gmail.com>
2017-01-28 19:30:50 +0530

    Builder Pattern

---
+++ src/com/learning/Builder/BuilderPatternExample.java
@@ -0,0 +1,17 @@
+package com.learning.Builder;
+
+public class BuilderPatternExample {
+
+	public static void main(String[] args) {
+		
+		StringBuilder builder = new StringBuilder();
+		builder.append("This is an example");
+		builder.append("of a builder pattern. ");
+		builder.append("It has methods to append");
+		builder.append("almost everything we want to a String. ");
+		builder.append(42);
+
+		System.out.println(builder.toString());
+//		System.out.println(builder.reverse());
+	}
+}
